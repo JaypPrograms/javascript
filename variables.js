b52b830a97793bbace5d6179513b071e9931273b
Jayperson Babaran <jaypersonbabaran31@gmail.com>
2024-02-12 06:51:48 +0800

    first commit on uploading my code to my cloud repo

---
+++ variables.js
@@ -0,0 +1,19 @@
+var firstName="jayp";
+var age=21;
+var isFemale=true;
+var balance=100.32;
+var dob= new Date (2000, 0, 20);
+var person ={}
+var empty=undefined
+
+
+
+console.log(firstName);
+console.log(age);
+console.log(isFemale);
+console.log(balance);
+console.log(dob);
+console.log(person);
+console.log(empty);
+
+
